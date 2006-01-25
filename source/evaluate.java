54ab31cfeceb478b4c2012d9e5716956aa339592
paulrho <Paul.Salanitri@gmail.com>
2006-01-25 06:50:22 +0000

    Pre 20060127

--- source/evaluate.java
+++ source/evaluate.java
@@ -11,6 +11,12 @@
 // infact, based on the last equation that didn't work in the former, and made me realise I'd gone way off track
 // works!
 
+// PGS20060125-1
+// added and and or
+// added chew up space
+
+// thoughts - what about strings and ints, run special modes perhaps??  - match in with c64
+
 class evaluate {
 
   String stkop[];
@@ -18,19 +24,41 @@ class evaluate {
   int upto;
   String stkfunc[];
   String printprefix="";
+  boolean verbose=true;
 
-  evaluate()
+  evaluate(String[] args)
   {
-    System.out.printf("Running evaluate\n");
+    if (false) { System.out.printf("Running evaluate\n"); }
     stknum=new double[100];
     stkop=new String[100];
     stkfunc=new String[100];
 
+    // what about functions with TWO or more arguments!!
+
+    verbose=false;
+    if (args.length>=1) {
+      interpret_string(args[0],0.0);
+    } else {
+
+    if (true) {
+    verbose=true;
+    // 100 x  82 equations = about 40 seconds, is : 205 equations per second on yoink
+    for (int i=0; i<1; ++i) {
+    interpret_string("0 or 2 and 0",0.0);
+    interpret_string("1 or 2 and 0",-1.0);
+    interpret_string("1 and 2 or 0",-1.0);
+    interpret_string("1 or 2",-1.0);
+    interpret_string("1or2",-1.0);
+    interpret_string("1and2",-1.0);
+    interpret_string("1and0",0.0);
+    interpret_string("1or0",-1.0);
+
     // errors on purpose
-    interpret_string("1+2)*3+1",0.0);
-    interpret_string("1+2*(3+1",0.0);
+    interpret_string("1+2)*3+1",10.0); //syntax wrong, but calculator wise definable
+    interpret_string("1+2*(3+1",9.0); //syntax wrong, but calculator wise definable
 
     // should work
+    //interpret_string("exp(10)",0.0);
     interpret_string("100+(1+(2+(3+(4+(5+(6+(7+8)))))))",100+1+2+3+4+5+6+7+8);
     interpret_string("7+(2*3+1+4*4)",7+(2*3+1+4*4));
     interpret_string("sin(1)",0.841471);
@@ -109,31 +137,46 @@ class evaluate {
     interpret_string("(1+2+3+4+5+6)",1+2+3+4+5+6);
     interpret_string("4*(1+2+3+4+5+6)",4*(1+2+3+4+5+6));
     interpret_string("(1+2+3+4+5+6)*4",4*(1+2+3+4+5+6));
-    if (false) {
     }
-  }
+    }
+    }
+  } // end func
 
   void show_state() {
-    System.out.printf("%sSTATE:  upto=%d\n",printprefix,upto);
+    System.out.printf("%s |STATE:  upto=%d doing=%d\n",printprefix,upto,doing);
     for (int levelx=0; levelx<upto; levelx++) {
       //System.out.printf("%supto=%d stknum[]=%f stkop[]=%s stkfunc[]=%s\n",printprefix,levelx,stknum[levelx],stkop[levelx],(stkop[levelx].equals("("))?stkfunc[levelx]:"N/A");
-      System.out.printf("%supto=%d stknum[]=%f stkop[]=%s%s%s\n",
+      System.out.printf("%s |upto=%d stknum[]=%f stkop[]=%s%s%s\n",
         printprefix,levelx,stknum[levelx],stkop[levelx],(stkfunc[levelx]!=null)?" stkfunc[]=":"",(stkfunc[levelx]!=null)?stkfunc[levelx]:"");
     }
   }
 
   // just a bit of shorthard to improve readability
-  double calc() {
-    return calc(stknum[upto-2],stkop[upto-2],stknum[upto-1],stkfunc[upto-2]);
+  void calc_and_pop() {
+    if (verbose) { System.out.printf("%sPerforming a calculation on %f %s %f\n",printprefix,stknum[upto-2],stkop[upto-2],stknum[upto-1]); }
+    stknum[upto-2]=calc(stknum[upto-2],stkop[upto-2],stknum[upto-1],stkfunc[upto-2]);
+      // where ( - tried stknum[upto-2]=calc(stknum[upto-2],stkop[upto-2],stknum[upto-1]); //efficiency only - but no better - I think
+    upto--;
+    return;
   }
 
   double calc(double left, String oper, double right, String function) {
     double answer=0.0;
-    if (oper.equals("(") && !function.equals("")) { 
+    if (oper.equals(OP_OPEN_BRACKET) && function != null && !function.equals("")) { 
       answer=right; 
-      System.out.printf("%sfunction needs calculating : %s(%f)\n",printprefix,function,right);
+      if (verbose) { System.out.printf("%sfunction needs calculating : %s(%f)\n",printprefix,function,right); }
       if (function.equals("sin")) {
         answer=Math.sin(right);
+      } else if (function.equals("sgn")) {
+        answer=0.0; // NI yet
+      } else if (function.equals("int")) {
+        answer=0.0; // NI yet
+      } else if (function.equals("abs")) {
+        answer=0.0; // NI yet
+      } else if (function.equals("rnd")) {
+        answer=0.0; // NI yet
+      } else if (function.equals("exp")) {
+        answer=Math.exp(right);
       } else if (function.equals("cos")) {
         answer=Math.cos(right);
       } else if (function.equals("tan")) {
@@ -143,7 +186,7 @@ class evaluate {
       } else if (function.equals("sqt")) { // for basic
         answer=Math.sqrt(right);
       } else {
-        System.out.printf("%sUnknown/unsupported function %s\n",printprefix,function);
+        System.out.printf("?SYTAX ERROR 003 *** Unknown/unsupported function %s\n",function);
       }
     
     } else answer=calc(left,oper,right);
@@ -151,7 +194,19 @@ class evaluate {
   }
 
   int prec(String oper) {
-    if (oper.equals("+")) { return 9; }
+         if (oper.equals("or")) { return 6; }
+    else if (oper.equals("and")) { return 7; }
+    else if (oper.equals(">")) { return 8; }
+    else if (oper.equals("<")) { return 8; }
+    else if (oper.equals("<=")) { return 8; }
+    else if (oper.equals("=<")) { return 8; }
+    else if (oper.equals(">=")) { return 8; }
+    else if (oper.equals("=>")) { return 8; }
+    else if (oper.equals("!=")) { return 8; }
+    else if (oper.equals("=")) { return 8; }
+    else if (oper.equals("==")) { return 8; }
+    else if (oper.equals("<>")) { return 8; }
+    else if (oper.equals("+")) { return 9; }
     else if (oper.equals("-")) { return 9; }
     else if (oper.equals("*")) { return 10; }
     else if (oper.equals("/")) { return 10; }
@@ -163,7 +218,9 @@ class evaluate {
   }
   double calc(double left, String oper, double right) {
     double answer=0.0;
-    if (oper.equals("+")) { answer=left+right; }
+         if (oper.equals("or")) { answer=(((left!=0.0)?true:false) || ((right!=0.0)?true:false))?-1.0:0.0; }
+    else if (oper.equals("and")) { answer=(((left!=0.0)?true:false) && ((right!=0.0)?true:false))?-1.0:0.0; }
+    else if (oper.equals("+")) { answer=left+right; }
     else if (oper.equals("-")) { answer=left-right; }
     else if (oper.equals("*")) { answer=left*right; }
     else if (oper.equals("/")) { answer=left/right; }
@@ -176,8 +233,8 @@ class evaluate {
       //answer=NaN;
       answer=0.0;
     }
-    System.out.printf("%sCalculating %f %s %f, Answer = %f\n",printprefix,left,oper,right,answer);
-    show_state();
+    if (verbose) { System.out.printf("%sCalculating %f %s %f, Answer = %f\n",printprefix,left,oper,right,answer); }
+    if (verbose) { show_state(); }
     return answer;
   }
 
@@ -197,6 +254,8 @@ class evaluate {
   int doing;
   static int D_NUM=0;
   static int D_OP=1;
+  static String OP_OPEN_BRACKET="(";
+  static String OP_CLOSE_BRACKET=")";
   String a; // temp string variable containing current pointed to char
   boolean is_function=false;
   String functionname="";
@@ -213,6 +272,7 @@ class evaluate {
     doing=D_NUM; // if you are going to push an op, it is ( and therefore next is num
   }
   void setOp(String op) {
+    if (verbose) { System.out.printf("%sGot %s oper\n",printprefix,op); }
     if (upto==0) {
       System.out.printf("?STACK ERROR  ** tried to set current operator stack when empty\n");
       return; // cant do this
@@ -220,15 +280,10 @@ class evaluate {
     // now come the tricks, as soon as this op has a lower or equal precedence,
     // pop and calc the two above!
     while (upto>1 && prec(op)<=prec(stkop[upto-2])) {
-       
-      System.out.printf("%sPerforming a calculation on %f %s %f\n",printprefix,stknum[upto-2],stkop[upto-2],stknum[upto-1]);
-
-      if (stkop[upto-2].equals("(")) {
+      if (stkop[upto-2].equals(OP_OPEN_BRACKET)) {
         break; // go no further that the brackets
       }
-      double val=calc(stknum[upto-2],stkop[upto-2],stknum[upto-1]);
-      stknum[upto-2]=val;
-      upto--; // pop off stack one level
+      calc_and_pop(); //overhead of func passed too
     }
     stkop[upto-1]=op;
     doing=D_NUM;
@@ -249,8 +304,28 @@ class evaluate {
             ispnt++;
           }
           double value=Double.parseDouble(building);
-          System.out.printf("%sGot value %f\n",printprefix,value);
+          if (verbose) { System.out.printf("%sGot value %f\n",printprefix,value); }
     return value;
+  }
+
+  void readStringOp() {
+          String building=a;
+          while (ispnt<intstring.length()-1) {
+            a=intstring.substring(ispnt+1,ispnt+2);
+            if (a.compareTo("a")>=0 && a.compareTo("z")<=0) {
+              building=building+a;
+            } else { break; }
+            ispnt++;
+          }
+          if (verbose) { System.out.printf("%sgot a string %s\n",printprefix,building); }
+          if (building.equals("or") || building.equals("and")) {
+            //operatortoken=building;
+            setOp(building);
+            return;
+          } else {
+            System.out.printf("?SYNTAX ERROR *** not a valid operator %s\n",building);
+            return;
+          }
   }
 
   void readString() {
@@ -262,17 +337,17 @@ class evaluate {
             } else { break; }
             ispnt++;
           }
-          System.out.printf("%sgot a string %s\n",printprefix,building);
+          if (verbose) { System.out.printf("%sgot a string %s\n",printprefix,building); }
           // if the next char is ( then it is a function, otherwise it is a variable
-          if (ispnt<intstring.length()-1 && (intstring.substring(ispnt+1,ispnt+2)).equals("(")) {
-            System.out.printf("%sgot a function %s\n",printprefix,building);
+          if (ispnt<intstring.length()-1 && (intstring.substring(ispnt+1,ispnt+2)).equals(OP_OPEN_BRACKET)) {
+            if (verbose) { System.out.printf("%sgot a function %s\n",printprefix,building); }
             functionname=building;
             is_function=true;
           } else {
-            System.out.printf("%sgot a variable %s\n",printprefix,building);
+            if (verbose) { System.out.printf("%sgot a variable %s\n",printprefix,building); }
             // replace the variable with the value of the variable
             double value=get_value(building);
-            System.out.printf("%sGot value %f\n",printprefix,value);
+            if (verbose) { System.out.printf("%sGot value %f\n",printprefix,value); }
             // push this value on stack
             pushNum(value);
             doing=D_OP;
@@ -288,22 +363,23 @@ class evaluate {
     doing=D_NUM;
 
     intstring=intstring_param; // this may be a dumb way, but it will make code more readable
-    System.out.printf("%sInterpreting %s\n",printprefix,intstring);
+    if (verbose) { System.out.printf("%sInterpreting %s\n",printprefix,intstring); }
     /* take it a character at a time */
     // i is now part of the object
     for (ispnt=0; ispnt<intstring.length() ;++ispnt) {
-      System.out.printf("%sGot character %s (index %d) [upto=%d, doing=%d]\n",printprefix,intstring.substring(ispnt,ispnt+1),ispnt,upto,doing);
+      if (verbose) { System.out.printf("%sGot character %s (index %d) [upto=%d, doing=%d]\n",printprefix,intstring.substring(ispnt,ispnt+1),ispnt,upto,doing); }
       a=intstring.substring(ispnt,ispnt+1);
+      if (a.equals(" ")) { continue; }
       // it is all different now:
       
       if (doing==D_NUM /* looking for number */) {
-        if (a.equals("(")) {
+        if (a.equals(OP_OPEN_BRACKET)) {
           pushOp(a); // (
           if (is_function) {
-            System.out.printf("%sSetting functionname to %s at upto-1=%d\n",printprefix,functionname,upto-1);
+            if (verbose) { System.out.printf("%sSetting functionname to %s at upto-1=%d\n",printprefix,functionname,upto-1); }
             stkfunc[upto-1]=functionname;
-          }  else { stkfunc[upto-1]=""; }
-          is_function=false;
+            is_function=false;
+          } else { stkfunc[upto-1]=""; } // must clear it as there is no function
         
         } else if (a.compareTo("0")>=0 && a.compareTo("9")<=0 || a.equals("-") || a.equals("+")) {
           double num=readNum();
@@ -315,80 +391,65 @@ class evaluate {
         } else {
           System.out.printf("?SYNTAX ERROR 001\n***Not correct syntax\n");
         }
-        show_state();
+        if (verbose) { show_state(); }
       } else if (doing==D_OP) {
-        if (a.equals("^")) {
-          System.out.printf("%sGot ^ oper\n",printprefix);
+        if (a.equals("^") || a.equals("*") || a.equals("/") || a.equals("+") || a.equals("-")) {
           setOp(a);
-        } else if (a.equals("*")||a.equals("/")) {
-          System.out.printf("%sGot */ oper\n",printprefix);
-          setOp(a);
-        } else if (a.equals("+")||a.equals("-")) {
-          System.out.printf("%sGot +- oper\n",printprefix);
-          setOp(a);
-        } else if (a.equals(")")) {
-          System.out.printf("%sCLOSING BRACKET from doing=%d\n",printprefix,doing);
+        } else if (a.equals(OP_CLOSE_BRACKET)) {
+          if (verbose) { System.out.printf("%sCLOSING BRACKET from doing=%d\n",printprefix,doing); }
            // pop eveything off until we get to an op of ( and then pop that too
           while (upto>1) {
-             
-            System.out.printf("%sPerforming a calculation on %f %s %f\n",printprefix,stknum[upto-2],stkop[upto-2],stknum[upto-1]);
-      
-            if (stkop[upto-2].equals("(")) {
-              System.out.printf("%sleading bracket found and upto-2\n",printprefix);
+            if (verbose) { System.out.printf("%sPerforming a calculation on %f %s %f\n",printprefix,stknum[upto-2],stkop[upto-2],stknum[upto-1]); }
+            if (stkop[upto-2].equals(OP_OPEN_BRACKET)) {
               // pop this too, but end
-              if (stkfunc[upto-2].equals("")) {
-                stknum[upto-2]=stknum[upto-1];
-              } else {
-                System.out.printf("%sfunction found %s\n",printprefix,stkfunc[upto-2]);
-                stknum[upto-2]=calc(stknum[upto-2],stkop[upto-2],stknum[upto-1],stkfunc[upto-2]);
-              }
+              calc_and_pop();
               doing=D_OP; // efectively poping the op
-              upto--;
               break;
             }
-            double val=calc(stknum[upto-2],stkop[upto-2],stknum[upto-1]);
-            stknum[upto-2]=val;
-            upto--; // pop off stack one level
+            calc_and_pop(); //overhead of func passed too
           }
-
-          //setOp(a);
+          // allow also multi charactor operators OR and AND
+        } else if (a.compareTo("a")>=0 && a.compareTo("z")<=0) {
+          readStringOp();
+        } else {
+          System.out.printf("?SYNTAX ERROR 002\n***Not correct syntax\n");
         }
-        show_state();
+        if (verbose) { show_state(); }
       }
     } /* for */
     /* final calc */
 
-    System.out.printf("%sFINAL CALCS\n",printprefix);
+    if (verbose) { System.out.printf("%sFINAL CALCS\n",printprefix); }
     while (upto>1) {
-       
-      System.out.printf("%sPerforming a calculation on %f %s %f\n",printprefix,stknum[upto-2],stkop[upto-2],stknum[upto-1]);
-
       // the only things that will need the function in it will be badly closed brackets, what is the overhead to do this?
-      double val=calc(stknum[upto-2],stkop[upto-2],stknum[upto-1],stkfunc[upto-2]); //add func for unclosed brackets
-      stknum[upto-2]=val;
-      upto--; // pop off stack one level
+      //add func for unclosed brackets
+      calc_and_pop();
     }
-    show_state();
-    System.out.printf("-------------------------------\n");
-    System.out.printf("Evaluated %-20s  ",intstring);
-    System.out.printf("%sanswer=%12f  expecting=%12f  difference=%12f",printprefix,stknum[0],expecting,stknum[0]-expecting);
-    //System.out.printf("%sanswer=%12f  expecting=%12f  difference=%12f",printprefix,left[level],expecting,left[level]-expecting);
-    if (stknum[0]-expecting>0.00001 || stknum[0]-expecting<-0.00001) {
-      System.out.printf(" !!**BAD**!!\n");
-      System.out.printf("\n   ***************DISCREPENCY***************\n");
-      System.out.printf("-------------------------------\n\n");
+    if (verbose) { show_state(); }
+    if (verbose) {
+      System.out.printf("-------------------------------\n");
+      System.out.printf("Evaluated %-20s  ",intstring);
+      System.out.printf("%sanswer=%12f  expecting=%12f  difference=%12f",printprefix,stknum[0],expecting,stknum[0]-expecting);
+      //System.out.printf("%sanswer=%12f  expecting=%12f  difference=%12f",printprefix,left[level],expecting,left[level]-expecting);
+      if (stknum[0]-expecting>0.00001 || stknum[0]-expecting<-0.00001) {
+        System.out.printf(" !!**BAD**!!\n");
+        System.out.printf("\n   ***************DISCREPENCY***************\n");
+        System.out.printf("-------------------------------\n\n");
+      } else {
+        System.out.printf(" OKAY\n");
+        System.out.printf("-------------------------------\n\n");
+        //return 0;
+      }
     } else {
-      System.out.printf(" OKAY\n");
-      System.out.printf("-------------------------------\n\n");
-      //return 0;
+      System.out.printf("Evaluated %-20s = %f\n",intstring,stknum[0]);
     }
 
     return;
   }
 
   public static void main(String[] args) {
-    System.out.printf("Mello Word\n");
-    new evaluate();
+    if (false) { System.out.printf("Mello Word\n"); }
+    new evaluate(args);
   }
 }
 
